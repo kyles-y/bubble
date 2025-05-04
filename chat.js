ff237d7a5b24857e7ba48092c06f3748ad432d34
kyles-y <kyles@wcymca.org>
2025-05-04 16:43:52 -0400

    Update chat.js

--- chat.js
+++ chat.js
@@ -8,8 +8,8 @@ link.style.textDecoration = 'none';
 const button = document.createElement('div');
 button.textContent = '💬';
 button.style.position = 'fixed';
-button.style.bottom = '5px';
-button.style.right = '5px';
+button.style.bottom = '15px';
+button.style.right = '15px';
 button.style.width = '33px';
 button.style.height = '33px';
 button.style.backgroundColor = '#0089D0';
@@ -23,7 +23,8 @@ button.style.opacity = '0';
 button.style.transition = 'opacity 1s ease';
 button.style.cursor = 'pointer';
 button.style.zIndex = '9999';
-button.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.2)'; // Optional: add a shadow
+button.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.2)'; 
+button.style.transform = 'translateY(2px)';
 
 // Append and fade in
 link.appendChild(button);
